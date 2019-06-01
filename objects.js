ca53a0ce8724b569b007fc8b2f38d8f1d22d8ac6
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 14:49:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   Prince: 'Purple Rain',
   LedZeppelin: 'Heartbreaker'
 }
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName = songTitle;
+  return playlist;
+}
